1947d8af6e45d5417f2bcc384f57650841afa0b0
Léo Millon <leo.millon@ekino.com>
2019-01-03 14:24:25 +0100

    Make publishing credentials optional

--- build.gradle.kts
+++ build.gradle.kts
@@ -71,8 +71,8 @@ subprojects {
                         val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
                         url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
 
-                        val ossrhUsername: String by project
-                        val ossrhPassword: String by project
+                        val ossrhUsername: String? by project
+                        val ossrhPassword: String? by project
                         credentials {
                             username = ossrhUsername
                             password = ossrhPassword
